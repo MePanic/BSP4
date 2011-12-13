2ba661b8c3b4c4d9c77fffddf93b2c55212c60ea
Fabian W <Mephisto_III@web.de>
2011-12-13 16:42:42 +0100

    wasn scheiß

--- src/Algo/BuddyAlgo.java
+++ src/Algo/BuddyAlgo.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class BuddyAlgo {
 
 	private int size;
-	static ArrayList<ArrayList<Integer>> blocks = null;
+	static LinkedList<ArrayList<Integer>> blocks = null;
 	static int[] blockSizePool = null;
 	
 	public BuddyAlgo(int size){	
 		if(!checkForPower(size)) throw new IllegalArgumentException("Size muss eine Zweierpotenz sein");	
 		this.size = size;
-		blocks = new ArrayList<ArrayList<Integer>>();
+		blocks = new LinkedList<ArrayList<Integer>>();
 		blocks.add(new ArrayList<Integer>());
 		blocks.get(0).add(0, 0);
 		blocks.get(0).add(1, size-1);
@@ -20,6 +20,7 @@ public class BuddyAlgo {
 	}
 	
 	public static int allocateBlock(int size){
+		recycle();
 		int tempBlockSize = size;
 		ArrayList<Integer> blockToAllocate = null;
 		if(!checkForPower(size)){ tempBlockSize = nextBlockSize(size);}
@@ -29,40 +30,52 @@ public class BuddyAlgo {
 		return blocks.indexOf(blockToAllocate);
 	}
 	
+	public static void deallocateBlock(int index){
+		
+		blocks.get(index).set(2, 0);
+	}
+	
 	public static ArrayList<Integer> splitBlocks(int size){
 		ArrayList<Integer> res = null;
 		ArrayList<Integer> lowBlock = null;
 		for(ArrayList<Integer> x : blocks){
 			if(x.get(1)-x.get(0)+1 == size && x.get(2) == 0){
-				return x;
-			} else if(x.get(1)-x.get(0)+1 > size && x.get(2) == 0 && lowBlock == null) {
-				lowBlock = x;			
-			} else if(x.get(1)-x.get(0)+1 > size && x.get(2) == 0 && lowBlock.get(1)-lowBlock.get(0)+1 > size){
-				lowBlock = x;				
+				return x;			
 			}
 		}
-		return splitBlocksHelp(lowBlock, size);
+		return splitBlocksHelp(smallestFittingBlock(size), size);
+	}
+	
+	public static ArrayList<Integer> smallestFittingBlock(int size){
+		ArrayList<Integer> res = null;
+		
+		for(ArrayList<Integer> x : blocks){
+			if(res == null && x.get(2) == 0){res = x;}
+			if(x.get(1)-x.get(0)+1 > size && x.get(2) == 0 && res.get(1)-res.get(0)+1 > x.get(1)-x.get(0)+1){res = x;}			
+		}
+		
+		return res;
 	}
 	
 	public static ArrayList<Integer> splitBlocksHelp(ArrayList<Integer> toSplit, int size){
-		ArrayList<Integer> result = null;
+		ArrayList<Integer> result = new ArrayList<Integer>();
+
 		if(toSplit.get(1)-toSplit.get(0)+1 == size){
 			result = toSplit;
 		} else {
 			ArrayList<Integer> temp1 = new ArrayList<Integer>();
-			ArrayList<Integer> temp2 = new ArrayList<Integer>();
-			
-			temp1.add(0, toSplit.get(0));
-			temp1.add(1, ((toSplit.get(1)+1)%2)-1);
-			temp1.add(2, 0);
-			temp1.add(0, (toSplit.get(1)+1)%2);
+
+
+			toSplit.set(0, toSplit.get(0));
+			toSplit.set(1, ((toSplit.get(1)+1)/2)-1);
+			toSplit.set(2, 0);
+			temp1.add(0, (toSplit.get(1)+1)/2);
 			temp1.add(1, toSplit.get(1));
 			temp1.add(2, 0);
+
+			blocks.add(blocks.indexOf(toSplit)+1, temp1);
 			
-			blocks.remove(toSplit);
-			blocks.add(temp1);
-			blocks.add(temp2);
-			result = splitBlocksHelp(temp2, size);
+			result = splitBlocksHelp(toSplit, size);
 			
 		}
 		return result;
@@ -90,6 +103,60 @@ public class BuddyAlgo {
 		int[] pool = new int[i];	
 		for(int k = 0; k < i; k++){ pool[k] = (int) Math.pow(2, k);}
 		return pool;
+	}
+	
+	public static void recycle(){
+		ArrayList<Integer> temp = null;	
+		LOOP:
+		
+
+		for(ArrayList<Integer> x : blocks){
+			if(x.get(2) == 0){
+				for(ArrayList<Integer> y : blocks){
+					if(y != x && y.get(1)+1 == x.get(0)&& y.get(2) == 0){
+						temp = new ArrayList<Integer>();
+						temp.add(0, y.get(0));
+						temp.add(1, x.get(1));
+						temp.add(2, 0);
+						
+						break LOOP;
+					}
+					
+					if(y != x && x.get(1)+1 == y.get(0) && y.get(2) == 0){
+						temp = new ArrayList<Integer>();
+						temp.add(0, x.get(0));
+						temp.add(1, y.get(1));
+						temp.add(2, 0);
+						
+						
+						break LOOP;
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	@Override
+	public String toString(){
+		String string = "";
+		int temp = 0;
+		while(temp != size){
+			for(ArrayList<Integer> x : blocks){
+				if(temp == x.get(0)){
+					System.out.print("[" + x.get(0) + ".." + x.get(2) + ".." + x.get(1) + "] ");
+					temp = x.get(1)+1;
+					break;
+				}
+				
+			}
+			
+		}
+		System.out.println();
+		return string;
 	}
 	
 }
--- src/Tests/AlgoTest.java
+++ src/Tests/AlgoTest.java
@@ -13,27 +13,40 @@ public class AlgoTest {
 		
 		BuddyAlgo blub = new BuddyAlgo(512);
 		
-		assertEquals(true, Algo.BuddyAlgo.checkForPower(4));
-		assertEquals(true, Algo.BuddyAlgo.checkForPower(8));
-		assertEquals(true, Algo.BuddyAlgo.checkForPower(16));
-		assertEquals(true, Algo.BuddyAlgo.checkForPower(32));
-		assertEquals(true, Algo.BuddyAlgo.checkForPower(64));
-		assertEquals(true, Algo.BuddyAlgo.checkForPower(128));
-		assertEquals(true, Algo.BuddyAlgo.checkForPower(4096));
-		assertEquals(false, Algo.BuddyAlgo.checkForPower(555));
-		assertEquals(false, Algo.BuddyAlgo.checkForPower(513));
-		assertEquals(false, Algo.BuddyAlgo.checkForPower(511));
-		assertEquals(false, Algo.BuddyAlgo.checkForPower(127));
-		assertEquals(false, Algo.BuddyAlgo.checkForPower(65));
+//		assertEquals(true, Algo.BuddyAlgo.checkForPower(4));
+//		assertEquals(true, Algo.BuddyAlgo.checkForPower(8));
+//		assertEquals(true, Algo.BuddyAlgo.checkForPower(16));
+//		assertEquals(true, Algo.BuddyAlgo.checkForPower(32));
+//		assertEquals(true, Algo.BuddyAlgo.checkForPower(64));
+//		assertEquals(true, Algo.BuddyAlgo.checkForPower(128));
+//		assertEquals(true, Algo.BuddyAlgo.checkForPower(4096));
+//		assertEquals(false, Algo.BuddyAlgo.checkForPower(555));
+//		assertEquals(false, Algo.BuddyAlgo.checkForPower(513));
+//		assertEquals(false, Algo.BuddyAlgo.checkForPower(511));
+//		assertEquals(false, Algo.BuddyAlgo.checkForPower(127));
+//		assertEquals(false, Algo.BuddyAlgo.checkForPower(65));
+//		
+//		for(int x : Algo.BuddyAlgo.calculateBlockPool(16)){
+//			System.out.println(x);
+//		}
+//		
+//		assertEquals(16, blub.nextBlockSize(12));
+//		assertEquals(512, blub.nextBlockSize(257));
+//		assertEquals(128, blub.nextBlockSize(65));
+//		assertEquals(64, blub.nextBlockSize(63));
 		
-		for(int x : Algo.BuddyAlgo.calculateBlockPool(16)){
-			System.out.println(x);
-		}
+		blub.allocateBlock(16);
+		blub.toString();
+		blub.allocateBlock(16);
+		blub.toString();
+		blub.allocateBlock(4);
+		blub.toString();
+		blub.allocateBlock(16);
+		blub.toString();
+		blub.allocateBlock(16);
+		blub.toString();
 		
-		assertEquals(16, blub.nextBlockSize(12));
-		assertEquals(512, blub.nextBlockSize(257));
-		assertEquals(128, blub.nextBlockSize(65));
-		assertEquals(64, blub.nextBlockSize(63));
+		
 	}
 
 }
